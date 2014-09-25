93d8b6f2a7177d3befd4346def94e9c0f9b7a6c1
joelkeeling <jkeeling@ualberta.ca>
2014-09-25 16:51:03 -0600

    created Summary activity and Archive Activity

--- A1TodoList/gen/ca/ualberta/cs/jkeeling/a1todolist/R.java
+++ A1TodoList/gen/ca/ualberta/cs/jkeeling/a1todolist/R.java
@@ -27,26 +27,36 @@ public final class R {
     public static final class id {
         public static final int AddButton=0x7f080003;
         public static final int AddTextBox=0x7f080001;
-        public static final int AllArchive=0x7f08000c;
-        public static final int AllEmail=0x7f08000d;
-        public static final int Archive=0x7f080009;
+        public static final int AllArchive=0x7f080011;
+        public static final int AllEmail=0x7f080012;
+        public static final int Archive=0x7f08000e;
         public static final int OptionsButton=0x7f080000;
-        public static final int SelectArchive=0x7f08000a;
-        public static final int SelectEmail=0x7f08000b;
-        public static final int Summary=0x7f080008;
+        public static final int SelectArchive=0x7f08000f;
+        public static final int SelectEmail=0x7f080010;
+        public static final int Summary=0x7f08000d;
         public static final int TDList=0x7f080002;
-        public static final int action_settings=0x7f080007;
-        public static final int checkBox=0x7f080004;
-        public static final int deleteButton=0x7f080006;
-        public static final int textView=0x7f080005;
+        public static final int action_settings=0x7f08000c;
+        public static final int checkBox=0x7f080009;
+        public static final int deleteButton=0x7f08000b;
+        public static final int textView=0x7f08000a;
+        public static final int textView1=0x7f080004;
+        public static final int textView2=0x7f080006;
+        public static final int textView3=0x7f080005;
+        public static final int textView4=0x7f080007;
+        public static final int textView5=0x7f080008;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int item_row=0x7f030001;
+        public static final int activity_archive=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int activity_summary=0x7f030002;
+        public static final int archive_row=0x7f030003;
+        public static final int item_row=0x7f030004;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
-        public static final int options_popup=0x7f070001;
+        public static final int archive=0x7f070000;
+        public static final int main=0x7f070001;
+        public static final int options_popup=0x7f070002;
+        public static final int summary=0x7f070003;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
@@ -61,6 +71,8 @@ public final class R {
         public static final int select_archive=0x7f050009;
         public static final int select_email=0x7f05000a;
         public static final int summary=0x7f050007;
+        public static final int title_activity_archive=0x7f05000d;
+        public static final int title_activity_summary=0x7f05000c;
     }
     public static final class style {
         /** 
