4a692d2b29fe716364f9a3df0c45e770f348ffb0
joelkeeling <jkeeling@ualberta.ca>
2014-09-26 01:20:38 -0600

    More Organization

--- A1TodoList/src/ca/ualberta/cs/jkeeling/a1todolist/data/FileDataManager.java
+++ A1TodoList/src/ca/ualberta/cs/jkeeling/a1todolist/data/FileDataManager.java
@@ -1,5 +1,4 @@
 package ca.ualberta.cs.jkeeling.a1todolist.data;
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -7,9 +6,7 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.List;
-
 import android.content.Context;
-import android.os.Environment;
 import android.util.Log;
 import ca.ualberta.cs.jkeeling.a1todolist.models.TDItem;
 
@@ -24,39 +21,17 @@ public class FileDataManager implements IDataManager{
 	
 	public ArrayList<TDItem> loadItems() {
 		ArrayList<TDItem> items = new ArrayList<TDItem>();
-
 		try {
 			FileInputStream fis = new FileInputStream(filename);
 			ObjectInputStream ois = new ObjectInputStream(fis);
-
 			items = (ArrayList<TDItem>) ois.readObject();
-
 		} catch (Exception e) {
 			Log.i("LonelyTwitter", "Error casting");
 			e.printStackTrace();
 		} 
-
 		return items;
-	}
-	/*
-	public void saveItems(List<TDItem> items) {
-		try {
-			File f = new File(FILENAME);
-			if (!f.exists()){
-				String path = f.getPath();
-				f.createNewFile();				
-			}
-			
-			FileOutputStream fos = new FileOutputStream(FILENAME);
-			ObjectOutputStream oos = new ObjectOutputStream(fos);
-			oos.writeObject(items);
-			fos.close();
-		} 
-		catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	*/
+	}	
+	
 	public void saveItems(List<TDItem> items) {
 		FileOutputStream fos;		
 		try {		
--- A1TodoList/src/ca/ualberta/cs/jkeeling/a1todolist/data/IDataManager.java
+++ A1TodoList/src/ca/ualberta/cs/jkeeling/a1todolist/data/IDataManager.java
@@ -1,7 +1,6 @@
 package ca.ualberta.cs.jkeeling.a1todolist.data;
 import java.util.ArrayList;
 import java.util.List;
-
 import ca.ualberta.cs.jkeeling.a1todolist.models.TDItem;
 
 public interface IDataManager {
