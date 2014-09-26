431332a9ebb2341dc32c120522eac54a977682d9
joelkeeling <jkeeling@ualberta.ca>
2014-09-26 01:10:58 -0600

    Reorganized Archive Activity

--- A1TodoList/src/ca/ualberta/cs/jkeeling/a1todolist/ArchiveActivity.java
+++ A1TodoList/src/ca/ualberta/cs/jkeeling/a1todolist/ArchiveActivity.java
@@ -1,13 +1,10 @@
 package ca.ualberta.cs.jkeeling.a1todolist;
-
 import java.util.ArrayList;
 import java.util.List;
-
 import ca.ualberta.cs.jkeeling.a1todolist.adapters.ItemAdapter;
 import ca.ualberta.cs.jkeeling.a1todolist.data.FileDataManager;
 import ca.ualberta.cs.jkeeling.a1todolist.models.CustomTextView;
 import ca.ualberta.cs.jkeeling.a1todolist.models.TDItem;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
@@ -20,8 +17,7 @@ import android.widget.ListView;
 import android.widget.PopupMenu;
 import android.widget.Toast;
 
-public class ArchiveActivity extends Activity {
-	
+public class ArchiveActivity extends Activity {	
 	private List<TDItem> archivedItemsList = new ArrayList<TDItem>();
 	private List<TDItem> allItemsList = new ArrayList<TDItem>();
 	private ItemAdapter adapter; 
@@ -67,44 +63,17 @@ public class ArchiveActivity extends Activity {
 		//Set adapter
 		adapter = new ItemAdapter(this, R.layout.archive_row, archivedItemsList);		
 		ListView listView = (ListView) findViewById(R.id.ArchiveList);
-		listView.setAdapter(adapter);
-				
+		listView.setAdapter(adapter);				
 	}	
 	
-	public void OptionsMenu(View v){
-		PopupMenu popup = new PopupMenu(this, optionsBtn);
-		popup.getMenuInflater().inflate(R.menu.archive_options_popup, popup.getMenu());
-		popup.show();
+	public void delete(View v){
+		TDItem target = getTargetItem(v);
+		archivedItemsList.remove(target);
+		allItemsList.remove(target);
+		fdm.saveItems(allItemsList);
+		adapter.notifyDataSetChanged();
 	}
 	
-	
-	public void toMain(MenuItem i){
-		Intent intent = new Intent(this, MainActivity.class);		
-		startActivity(intent);
-	}
-	
-	public void toSummary(MenuItem i){
-		Intent intent = new Intent(this, SummaryActivity.class);		
-		startActivity(intent);
-	}
-	
-	public List<TDItem> getSelected(){
-		List<TDItem> selectedItemsList = new ArrayList<TDItem>();
-		for (TDItem item : archivedItemsList){
-			if (item.getSelected() == true){
-				selectedItemsList.add(item);
-			}
-		}
-		return selectedItemsList;
-	}
-	
-	public void unarchiveAll(MenuItem i){
-		for (TDItem item : archivedItemsList){
-			item.setSelected(true);
-		}
-		unarchiveSelected(i);
-	}
-		
 	public void select(View v){
 		TDItem target = getTargetItem(v);
 		if (target.toggleSelected() == true){
@@ -114,7 +83,30 @@ public class ArchiveActivity extends Activity {
 			((View)v.getParent()).setBackgroundColor(Color.WHITE);
 		}
 		fdm.saveItems(allItemsList);
+	}	
+	
+	public void optionsMenu(View v){
+		PopupMenu popup = new PopupMenu(this, optionsBtn);
+		popup.getMenuInflater().inflate(R.menu.archive_options_popup, popup.getMenu());
+		popup.show();
 	}
+	
+	public void toSummary(MenuItem i){
+		Intent intent = new Intent(this, SummaryActivity.class);		
+		startActivity(intent);
+	}
+	
+	public void toMain(MenuItem i){
+		Intent intent = new Intent(this, MainActivity.class);		
+		startActivity(intent);
+	}	
+	
+	public void unarchiveAll(MenuItem i){
+		for (TDItem item : archivedItemsList){
+			item.setSelected(true);
+		}
+		unarchiveSelected(i);
+	}	
 	
 	public void unarchiveSelected(MenuItem i){
 		List<TDItem> itemList = getSelected();
@@ -126,27 +118,7 @@ public class ArchiveActivity extends Activity {
 		fdm.saveItems(allItemsList);
 		adapter.notifyDataSetChanged();
 	}
-	
-	public void Delete(View v){
-		TDItem target = getTargetItem(v);
-		archivedItemsList.remove(target);
-		allItemsList.remove(target);
-		fdm.saveItems(allItemsList);
-		adapter.notifyDataSetChanged();
-	}
-	
-	public TDItem getTargetItem(View v){
-		View parent = (View)v.getParent();			
-		CustomTextView text = (CustomTextView) parent.findViewById(R.id.textView);
-		String uId = (String) text.getUId();
-		TDItem target = null;
-		for (TDItem item : archivedItemsList){
-			if (item.getId().equals(uId)){
-				target = item;
-			}
-		}
-		return target;
-	}
+			
 	public void emailAll(MenuItem i){
 		for (TDItem item : allItemsList){
 			item.setSelected(true);
@@ -198,5 +170,28 @@ public class ArchiveActivity extends Activity {
 		} catch (android.content.ActivityNotFoundException ex) {
 		    Toast.makeText(this, "There are no email clients installed.", Toast.LENGTH_SHORT).show();
 		}
+	}
+	
+	public List<TDItem> getSelected(){
+		List<TDItem> selectedItemsList = new ArrayList<TDItem>();
+		for (TDItem item : archivedItemsList){
+			if (item.getSelected() == true){
+				selectedItemsList.add(item);
+			}
+		}
+		return selectedItemsList;
+	}
+	
+	public TDItem getTargetItem(View v){
+		View parent = (View)v.getParent();			
+		CustomTextView text = (CustomTextView) parent.findViewById(R.id.textView);
+		String uId = (String) text.getUId();
+		TDItem target = null;
+		for (TDItem item : archivedItemsList){
+			if (item.getId().equals(uId)){
+				target = item;
+			}
+		}
+		return target;
 	}
 }
